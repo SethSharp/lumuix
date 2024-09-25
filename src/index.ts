bf948b948ce92087be918fc157cdb24458ab6810
Seth Sharp <sesharp@outlook.com>
2024-09-25 14:14:18 +1000

    clean up index.ts file

--- src/index.ts
+++ src/index.ts
@@ -4,25 +4,6 @@ import PrimaryButton from './components/buttons/PrimaryButton.vue'
 import SecondaryButton from './components/buttons/SecondaryButton.vue'
 import DangerButton from './components/buttons/DangerButton.vue'
 
-// form
-import { Form, FormGrid, FormElement, Error } from '@/components/form'
-
-// inputs
-import {
-  CharacterCount,
-  Checkbox,
-  DatePicker,
-  Description,
-  Base,
-  Text,
-  Label,
-  Combobox,
-  ImageUpload,
-  Select,
-  TextArea,
-  Toggle,
-} from '@/components/inputs'
-
 // pre-built
 import Datatable from './components/pre-built/Datatable.vue'
 import DarkModeToggle from './components/pre-built/DarkModeToggle.vue'
@@ -33,237 +14,25 @@ import Modal from './components/pre-built/Modal.vue'
 import Notifications from './components/page/Notifications.vue'
 import Tabs from './components/pre-built/Tabs.vue'
 
-// tabs
-import { TabsRoot, TabsTrigger, TabsItem, TabsList, TabsContent } from '@/components/tabs'
-
-// table
-import {
-  CollapsibleTableRow,
-  Table,
-  TableBody,
-  TableCell,
-  TableHeader,
-  TableHead,
-  TableFooter,
-  TableRow,
-  TableRowAction,
-  TableCaption,
-  TableEmpty,
-} from '@/components/table'
-
-// select
-import {
-  SelectRoot,
-  SelectValue,
-  SelectTrigger,
-  SelectGroup,
-  SelectItem,
-  SelectItemText,
-  SelectLabel,
-  SelectSeparator,
-  SelectScrollUpButton,
-  SelectScrollDownButton,
-  SelectContent,
-} from '@/components/select'
-
-// popover
-import { Popover, PopoverTrigger, PopoverContent } from '@/components/popover'
-
-// pagination
-import {
-  PaginationRoot,
-  PaginationList,
-  PaginationListItem,
-  PaginationEllipsis,
-  PaginationFirst,
-  PaginationLast,
-  PaginationNext,
-  PaginationPrev,
-  PaginationDetails,
-} from '@/components/pagination'
-
-// dropdown-menu
-import {
-  DropdownMenuPortal,
-  DropdownMenu,
-  BaseDropdownMenuItem,
-  DropdownMenuLink,
-  DropdownMenuTrigger,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuRadioGroup,
-  DropdownMenuItem,
-  DropdownMenuCheckboxItem,
-  DropdownMenuRadioItem,
-  DropdownMenuShortcut,
-  DropdownMenuSeparator,
-  DropdownMenuLabel,
-  DropdownMenuSub,
-  DropdownMenuSubTrigger,
-  DropdownMenuSubContent,
-} from '@/components/dropdown-menu'
-
-// dialog
-import {
-  Dialog,
-  DialogClose,
-  DialogTrigger,
-  DialogHeader,
-  DialogTitle,
-  DialogDescription,
-  DialogContent,
-  DialogScrollContent,
-  DialogFooter,
-} from '@/components/dialog'
-
-// command
-import {
-  Command,
-  CommandDialog,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList,
-  CommandSeparator,
-  CommandShortcut,
-} from '@/components/command'
-
-// calendar
-import {
-  CalendarCell,
-  CalendarCellTrigger,
-  CalendarGrid,
-  CalendarGridBody,
-  CalendarGridHead,
-  CalendarGridRow,
-  CalendarHeadCell,
-  CalendarHeader,
-  CalendarHeading,
-  CalendarNextButton,
-  CalendarPrevButton,
-} from '@/components/calendar'
+export * from '@/components/tabs'
+export * from '@/components/table'
+export * from '@/components/select'
+export * from '@/components/popover'
+export * from '@/components/pagination'
+export *from '@/components/dropdown-menu'
+export * from '@/components/dialog'
+export * from '@/components/command'
+export * from '@/components/calendar'
+export * from '@/components/form'
+export * from '@/components/inputs'
 
 export {
-  // buttons
   Button,
   PrimaryButton,
   SecondaryButton,
   DangerButton,
-  // inputs
-  CharacterCount,
-  Combobox,
-  Checkbox,
-  Error,
-  Base,
-  DatePicker,
-  Description,
-  Form,
-  FormGrid,
-  FormElement,
-  ImageUpload,
-  Label,
-  Select,
-  TextArea,
-  Text,
-  Toggle,
-  // popover
-  Popover,
-  PopoverTrigger,
-  PopoverContent,
-  // table
-  Table,
-  TableBody,
-  TableCell,
-  TableHeader,
-  TableHead,
-  TableFooter,
-  TableRow,
-  TableRowAction,
-  TableCaption,
-  TableEmpty,
-  CollapsibleTableRow,
-  // select
-  SelectRoot,
-  SelectValue,
-  SelectTrigger,
-  SelectGroup,
-  SelectItem,
-  SelectItemText,
-  SelectLabel,
-  SelectSeparator,
-  SelectScrollUpButton,
-  SelectScrollDownButton,
-  SelectContent,
-  // select
-  PaginationRoot,
-  PaginationList,
-  PaginationListItem,
-  PaginationEllipsis,
-  PaginationFirst,
-  PaginationLast,
-  PaginationNext,
-  PaginationPrev,
-  PaginationDetails,
-  // dropdown
-  DropdownMenuPortal,
-  DropdownMenu,
-  BaseDropdownMenuItem,
-  DropdownMenuLink,
-  DropdownMenuTrigger,
-  DropdownMenuContent,
-  DropdownMenuRadioGroup,
-  DropdownMenuItem,
-  DropdownMenuCheckboxItem,
-  DropdownMenuRadioItem,
-  DropdownMenuShortcut,
-  DropdownMenuSeparator,
-  DropdownMenuLabel,
-  DropdownMenuSub,
-  DropdownMenuSubTrigger,
-  DropdownMenuSubContent,
-  DropdownMenuGroup,
-  // dialog
-  Dialog,
-  DialogClose,
-  DialogTrigger,
-  DialogHeader,
-  DialogTitle,
-  DialogDescription,
-  DialogContent,
-  DialogScrollContent,
-  DialogFooter,
-  // command
-  Command,
-  CommandDialog,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList,
-  CommandSeparator,
-  CommandShortcut,
-  // calendar
-  CalendarCell,
-  CalendarCellTrigger,
-  CalendarGrid,
-  CalendarGridBody,
-  CalendarGridHead,
-  CalendarGridRow,
-  CalendarHeadCell,
-  CalendarHeader,
-  CalendarHeading,
-  CalendarNextButton,
-  CalendarPrevButton,
   Notifications,
-  // tabs
   Tabs,
-  TabsRoot,
-  TabsTrigger,
-  TabsItem,
-  TabsList,
-  TabsContent,
-  // pre-built
   Pagination,
   Modal,
   DarkModeToggle,

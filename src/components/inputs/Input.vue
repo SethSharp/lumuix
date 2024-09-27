63a21d5602423185996240066e0eb2920b2f45cf
Seth Sharp <sesharp@outlook.com>
2024-09-27 13:00:34 +1000

    remove ring, make border slate on focus

--- src/components/inputs/Input.vue
+++ src/components/inputs/Input.vue
@@ -23,7 +23,7 @@ const input = defineModel()
     :placeholder="placeholder"
     :required="required"
     :type="type"
-    class="flex h-10 w-full rounded-md border border-slate-200 bg-white px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:bg-slate-950 dark:text-slate-300 dark:ring-offset-slate-950 dark:placeholder:text-slate-500"
+    class="flex h-10 w-full rounded-md border border-slate-200 bg-white px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-slate-500 focus-visible:outline-none focus-visible:border-slate-900 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:bg-slate-950 dark:text-slate-300 dark:focus-visible:border-slate-300 dark:placeholder:text-slate-500"
     :class="{
       'focus-visible:ring-slate-950 dark:focus-visible:ring-slate-400': !error,
       'focus-visible:ring-red-600 dark:focus-visible:ring-red-400': error,
--- src/components/inputs/TextArea.vue
+++ src/components/inputs/TextArea.vue
@@ -22,7 +22,7 @@ const modelValue = useVModel(props, 'modelValue', emits, {
   <textarea
     v-model="modelValue"
     :placeholder="placeholder"
-    class="flex min-h-20 w-full rounded-md border border-slate-200 bg-white px-3 py-2 text-sm ring-offset-white placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:bg-slate-950 dark:text-slate-300 dark:ring-offset-slate-950 dark:placeholder:text-slate-500"
+    class="flex min-h-20 w-full rounded-md border border-slate-200 bg-white px-3 py-2 text-sm ring-offset-white placeholder:text-slate-500 focus-visible:outline-none focus-visible:border-slate-950 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:bg-slate-950 dark:text-slate-300 dark:focus-visible:border-slate-300  dark:placeholder:text-slate-500"
     :class="{
       'focus-visible:ring-slate-950 dark:focus-visible:ring-slate-400': !error,
       'focus-visible:ring-red-600 dark:focus-visible:ring-red-400': error,

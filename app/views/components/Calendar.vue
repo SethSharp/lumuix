7bcc46cca9d43605087836d8d74cd4b1f7c17e1c
Seth Sharp <sesharp@outlook.com>
2024-10-04 13:50:01 +1000

    move Calendar to /calendar

--- app/views/components/Calendar.vue
+++ app/views/components/Calendar.vue
@@ -1,5 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { type Ref, ref, watch } from 'vue'
+import { type DateValue, getLocalTimeZone, today } from '@internationalized/date'
+import { Calendar } from '@/components/calendar'
+
+const value = ref(today(getLocalTimeZone())) as Ref<DateValue>
+
+watch(value, () => {
+  console.log(value.value)
+})
+</script>
 
 <template>
-  <div>Calendar</div>
+  <Calendar v-model="value" :weekday-format="'short'" class="rounded-md border w-fit" />
 </template>

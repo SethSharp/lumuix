6fd468d20e49fff92e2f216782b0932b590f43f9
Seth Sharp <sesharp@outlook.com>
2024-10-06 22:15:54 +1000

    wip

--- app/views/components/Calendar.vue
+++ app/views/components/Calendar.vue
@@ -1,15 +1,14 @@
 <script setup lang="ts">
-import { type Ref, ref, watch } from 'vue'
+import { type Ref, ref } from 'vue'
 import { type DateValue, getLocalTimeZone, today } from '@internationalized/date'
 import { Calendar } from '@/components/calendar'
 
 const value = ref(today(getLocalTimeZone())) as Ref<DateValue>
-
-watch(value, () => {
-  console.log(value.value)
-})
 </script>
 
 <template>
-  <Calendar v-model="value" :weekday-format="'short'" class="rounded-md border w-fit" />
+  <Calendar
+    v-model="value"
+    :weekday-format="'short'"
+    class="w-fit rounded-md border" />
 </template>
--- app/views/getting-started/Installation.vue
+++ app/views/getting-started/Installation.vue
@@ -1,19 +1,18 @@
 <script setup lang="ts">
-import MainLayout from '../../layouts/MainLayout.vue'
 import Code from '../../components/Code.vue'
+import MainLayout from '../../layouts/MainLayout.vue'
 </script>
 
 <template>
   <MainLayout title="Installation">
     <p>
-      Installation is fairly straight forward. First you must install the project from the npm registry:
+      Installation is fairly straight forward. First you must install the project from the npm
+      registry:
     </p>
 
     <Code> npm i @sethsharp/lumuix </Code>
 
-    <p>
-      Next step is to include the package in the tailwind.config.js purge configuration:
-    </p>
+    <p>Next step is to include the package in the tailwind.config.js purge configuration:</p>
 
     <pre>
   content: [

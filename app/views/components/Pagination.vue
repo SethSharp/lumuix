d8de5fa4c3e1b4e113e2cb075687c9be9af71b93
Seth Sharp <sesharp@outlook.com>
2024-10-07 16:12:59 +1000

    wip docs

--- app/views/components/Pagination.vue
+++ app/views/components/Pagination.vue
@@ -1,5 +1,50 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {
+  PaginationRoot,
+  PaginationEllipsis,
+  PaginationFirst,
+  PaginationLast,
+  PaginationList,
+  PaginationListItem,
+  PaginationNext,
+  PaginationPrev,
+} from '@/components/pagination'
+import { Button } from '@/components/button'
+</script>
 
 <template>
-  <div>Paginaiton</div>
+  <PaginationRoot
+    v-slot="{ page }"
+    :total="100"
+    :sibling-count="1"
+    show-edges
+    :default-page="2">
+    <PaginationList
+      v-slot="{ items }"
+      class="flex items-center gap-1">
+      <PaginationFirst />
+      <PaginationPrev />
+
+      <template v-for="(item, index) in items">
+        <PaginationListItem
+          v-if="item.type === 'page'"
+          :key="index"
+          :value="item.value"
+          as-child>
+          <Button
+            class="h-10 w-10 p-0"
+            :variant="item.value === page ? 'default' : 'outline'">
+            {{ item.value }}
+          </Button>
+        </PaginationListItem>
+        <PaginationEllipsis
+          v-else
+          :key="item.type"
+          :index="index" />
+      </template>
+
+      <PaginationNext />
+      <PaginationLast />
+    </PaginationList>
+  </PaginationRoot>
 </template>

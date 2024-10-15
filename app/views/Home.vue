e8e19ad1bc30bfd9dc68d22d2f078b5d7c9811c7
Seth Sharp <sesharp@outlook.com>
2024-10-16 07:39:07 +1000

    get started button on home page

--- app/views/Home.vue
+++ app/views/Home.vue
@@ -1,8 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Button } from '@/components/button'
+</script>
 
 <template>
   <div>
     This is a generic and relatively un-opinionated library. Offering components from `shadcn` to
     build form and page components for consuming apps.
+
+    <Button as-child>
+      <router-link to="/getting-started/installation"> Get Started </router-link>
+    </Button>
   </div>
 </template>

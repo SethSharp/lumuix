8b89336740d349bb5606f3cde85cc8cc185abef5
Seth Sharp <sesharp@outlook.com>
2024-11-02 11:30:58 +1000

    slider

---
+++ app/views/components/Slider.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { Slider } from '@/components/slider'
+import { cn } from '@/lib/utils'
+import { ref } from 'vue'
+
+const modelValue = ref([50])
+</script>
+
+<template>
+  <div class="mt-6 flex w-full flex-col justify-center">
+    <span class="mx-auto"> {{ modelValue?.[0] }}% </span>
+    <Slider
+      v-model="modelValue"
+      :max="100"
+      :step="1" />
+  </div>
+</template>

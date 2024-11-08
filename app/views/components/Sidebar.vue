07f003b4c3bdc87e7a091d514148e064f69f6889
Seth Sharp <sesharp@outlook.com>
2024-11-08 22:39:55 +1000

    wip

---
+++ app/views/components/Sidebar.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+// Sidebar is currently a WIP with 1/2 parts merged. Will hold off on it for now
+// https://github.com/unovue/shadcn-vue/pull/827
+</script>
+
+<template></template>
+
+<style scoped></style>

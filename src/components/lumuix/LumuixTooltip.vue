c61ddfeac18cb03c19d03e787e94e1b4ed3b5c52
Seth Sharp <sesharp@outlook.com>
2024-11-12 13:31:22 +1000

    add base lumuix tooltip

---
+++ src/components/lumuix/LumuixTooltip.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/tooltip'
+</script>
+
+<template>
+  <TooltipProvider>
+    <Tooltip>
+      <TooltipTrigger>
+        <slot />
+      </TooltipTrigger>
+      <TooltipContent align="center">
+        <slot name="tool-tip" />
+      </TooltipContent>
+    </Tooltip>
+  </TooltipProvider>
+</template>

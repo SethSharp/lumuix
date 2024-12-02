380be7a87dbd3444defcf373990131b6971aae01
Seth Sharp <sesharp@outlook.com>
2024-12-02 12:59:35 +1000

    add/use breadcrumbs in documentation

--- app/layouts/ComponentLayout.vue
+++ app/layouts/ComponentLayout.vue
@@ -2,6 +2,7 @@
 import { MoveRight } from 'lucide-vue-next'
 import MainLayout from './MainLayout.vue'
 import { Button } from '@/components/button'
+import { BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbSeparator } from '@'
 
 defineProps<{
   title: string
@@ -10,6 +11,19 @@ defineProps<{
 
 <template>
   <MainLayout :title="$route.name">
+    <template #breadcrumbs>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          Components
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbLink :href="`/components/${$route.name.toLowerCase()}`">
+            {{ $route.name }}
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    </template>
     <Button
       v-if="$route.meta.shadcn"
       variant="default"

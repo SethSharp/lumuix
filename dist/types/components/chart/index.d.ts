217c7be4a7454a5795c5b843966535c1265f3839
Seth Sharp <sesharp@outlook.com>
2025-01-02 08:23:57 +1000

    remove

--- dist/types/components/chart/index.d.ts
+++
@@ -1,6 +0,0 @@
-export { default as ChartCrosshair } from './ChartCrosshair.vue';
-export { default as ChartLegend } from './ChartLegend.vue';
-export { default as ChartSingleTooltip } from './ChartSingleTooltip.vue';
-export { default as ChartTooltip } from './ChartTooltip.vue';
-export declare function defaultColors(count?: number): string[];
-export * from './interface';

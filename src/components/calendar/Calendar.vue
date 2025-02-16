4d52ebce0a0762a8b087aaaa8cad07138351070a
Seth Sharp <sesharp@outlook.com>
2025-02-17 09:03:29 +1000

    calendarwithselect

--- src/components/calendar/Calendar.vue
+++ src/components/calendar/Calendar.vue
@@ -40,11 +40,9 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits)
     :class="cn('p-3', props.class)"
     v-bind="forwarded">
     <CalendarHeader>
-      <slot name="header">
-        <CalendarPrevButton />
-        <CalendarHeading />
-        <CalendarNextButton />
-      </slot>
+      <CalendarPrevButton />
+      <CalendarHeading />
+      <CalendarNextButton />
     </CalendarHeader>
 
     <div class="mt-4 flex flex-col gap-y-4 sm:flex-row sm:gap-x-4 sm:gap-y-0">
--- src/components/inputs/DatePicker.vue
+++ src/components/inputs/DatePicker.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { onMounted, ref, watch } from 'vue'
 import { CalendarIcon } from 'lucide-vue-next'
-import { createDecade, createYear, toDate } from 'radix-vue/date'
 import { DateFormatter, type DateValue, getLocalTimeZone } from '@internationalized/date'
 import { Base } from '@/components/inputs'
 import { Button } from '@/components/button'
@@ -56,59 +55,7 @@ onMounted(() => {
           v-model="value as DateValue | undefined"
           :default-value="defaultValue"
           :multiple="false"
-          initial-focus>
-          <template #header>
-            <CalendarHeader>
-              <CalendarHeading class="flex w-full items-center justify-between gap-2">
-                <Select
-                  :default-value="placeholder.month.toString()"
-                  @update:model-value="(v) => {
-                    if (!v || !placeholder) return;
-                    if (Number(v) === placeholder?.month) return;
-                      placeholder = placeholder.set({
-                        month: Number(v),
-                      })
-                    }"
-                  >
-                  <SelectTrigger aria-label="Select month" class="w-[60%]">
-                    <SelectValue placeholder="Select month" />
-                  </SelectTrigger>
-                  <SelectContent class="max-h-[200px]">
-                    <SelectItem
-                      v-for="month in createYear({ dateObj: date })"
-                      :key="month.toString()" :value="month.month.toString()"
-                    >
-                      {{ formatter.custom(toDate(month), { month: 'long' }) }}
-                    </SelectItem>
-                  </SelectContent>
-                </Select>
-
-                <Select
-                  :default-value="placeholder.year.toString()"
-                  @update:model-value="(v) => {
-                    if (!v || !placeholder) return;
-                      if (Number(v) === placeholder?.year) return;
-                      placeholder = placeholder.set({
-                        year: Number(v),
-                      })
-                    }"
-                  >
-                  <SelectTrigger aria-label="Select year" class="w-[40%]">
-                    <SelectValue placeholder="Select year" />
-                  </SelectTrigger>
-                  <SelectContent class="max-h-[200px]">
-                    <SelectItem
-                      v-for="yearValue in createDecade({ dateObj: date, startIndex: -10, endIndex: 10 })"
-                      :key="yearValue.toString()" :value="yearValue.year.toString()"
-                    >
-                      {{ yearValue.year }}
-                    </SelectItem>
-                  </SelectContent>
-                </Select>
-              </CalendarHeading>
-            </CalendarHeader>
-          </template>
-        </Calendar>
+          initial-focus />
       </PopoverContent>
     </Popover>
   </Base>

d821278e56950c0d3e8bab0a83dcb598227618ed
Seth Sharp <sesharp@outlook.com>
2025-05-16 10:42:32 +1000

    move components to ui/, define @sethsharp/lumuix as local ui/

--- documentation/src/App.vue
+++ documentation/src/App.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import { Button } from '@sethsharp/lumuix'
 </script>
 
 <template>
+  <Button>
+    Test me!
+  </Button>
   <RouterView />
 </template>

0ab74717cb3b934b10df5cf5f20d3184fa7625dc
SethSharp <58869086+SethSharp@users.noreply.github.com>
2025-05-17 08:01:02 +1000

    es module import on defaulttheme

--- documentation/tailwind.config.js
+++ documentation/tailwind.config.js
@@ -1,4 +1,4 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+import defaultTheme from 'tailwindcss/defaultTheme'
 import { preset } from '@sethsharp/lumuix'
 
 /** @type {import('tailwindcss').Config} */

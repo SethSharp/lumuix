791f979bb4a9265daea6b1f0bc11577f226dc6ac
SethSharp <58869086+SethSharp@users.noreply.github.com>
2025-05-17 14:43:39 +1000

    wip

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -3,10 +3,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 import { fileURLToPath, URL } from 'node:url'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     vue(),
+    tailwindcss(),
     viteStaticCopy({
       targets: [
         {

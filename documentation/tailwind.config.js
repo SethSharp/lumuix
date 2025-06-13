6cda52908f47534c754c409559539ac314e9481e
Seth Sharp <58869086+SethSharp@users.noreply.github.com>
2025-06-13 10:34:16 +1000

    got it

---
+++ documentation/tailwind.config.js
@@ -0,0 +1,22 @@
+
+import defaultTheme from 'tailwindcss/defaultTheme'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+    './node_modules/@sethsharp/lumuix/dist/*.js',
+  ],
+
+  safelist: ['max-w-sm', 'max-w-md', 'max-w-lg', 'max-w-xl', 'max-w-2xl'],
+
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Rubik', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+}
--- documentation/vite.config.ts
+++ documentation/vite.config.ts
@@ -1,7 +1,8 @@
-import { fileURLToPath, URL } from 'node:url'
-import tailwindcss from "@tailwindcss/vite"
+import path from 'node:path'
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,7 +12,8 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': path.resolve(__dirname, './src'),
+      '@sethsharp/lumuix/presets/slate.css': resolve(__dirname, '../ui/dist/presets/slate.css')
     },
   },
   build: {

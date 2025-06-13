08c81ac7aa86028f9dd118fc4bc0fa327c3ba514
Seth Sharp <58869086+SethSharp@users.noreply.github.com>
2025-06-13 14:32:13 +1000

    update rollup to per directory

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -19,19 +19,48 @@ export default defineConfig({
     })
   ],
   build: {
+    emptyOutDir: false,
+    outDir: "dist",
+    sourcemap: true,
     lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'lumuix',
-      fileName: 'lumuix',
+      entry: {
+        'accordion': './src/components/accordion/index.ts',
+        'badge': './src/components/badge/index.ts',
+        'breadcrumb': './src/components/breadcrumb/index.ts',
+        'button': './src/components/button/index.ts',
+        'calendar': './src/components/calendar/index.ts',
+        'card': './src/components/card/index.ts',
+        'combobox': './src/components/combobox/index.ts',
+        'command': './src/components/command/index.ts',
+        'dialog': './src/components/dialog/index.ts',
+        'dropdown-menu': './src/components/dropdown-menu/index.ts',
+        'form': './src/components/form/index.ts',
+        'inputs': './src/components/inputs/index.ts',
+        'lumuix': './src/components/lumuix/index.ts',
+        'pagination': './src/components/pagination/index.ts',
+        'popover': './src/components/popover/index.ts',
+        'range-calendar': './src/components/range-calendar/index.ts',
+        'select': './src/components/select/index.ts',
+        'separator': './src/components/separator/index.ts',
+        'sheet': './src/components/sheet/index.ts',
+        'sidebar': './src/components/sidebar/index.ts',
+        'skeleton': './src/components/skeleton/index.ts',
+        'slider': './src/components/slider/index.ts',
+        'table': './src/components/table/index.ts',
+        'tabs': './src/components/tabs/index.ts',
+        'toast': './src/components/toast/index.ts',
+        'tooltip': './src/components/tooltip/index.ts',
+      },
+      formats: ["es", "cjs"],
     },
     rollupOptions: {
       external: ['vue'],
     },
-    emptyOutDir: false,
   },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@/components': resolve(__dirname, 'src/components'),
     },
   },
 })

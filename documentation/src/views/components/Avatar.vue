a8e31ff5e5de6dffb268aace2cf5add5f2932dff
Seth Sharp <58869086+SethSharp@users.noreply.github.com>
2025-07-04 13:12:48 +1000

    Add avatar files

---
+++ documentation/src/views/components/Avatar.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import AvatarCode from '../code/AvatarCode.vue'
+import sourceCode from '../code/AvatarCode.vue?raw'
+import RenderPreviewCode from '../../components/RenderPreviewCode.vue'
+</script>
+
+<template>
+  <RenderPreviewCode :source-code="sourceCode">
+    <AvatarCode />
+  </RenderPreviewCode>
+</template>
--- ui/src/index.ts
+++ ui/src/index.ts
@@ -1,5 +1,6 @@
 export * from '@/components/accordion'
 export * from '@/components/alert'
+export * from '@/components/avatar'
 export * from '@/components/badge'
 export * from '@/components/breadcrumb'
 export * from '@/components/button'

01188a12fad4a04869c3ba4a8730819c9da9094a
SethSharp <58869086+SethSharp@users.noreply.github.com>
2025-07-12 12:36:57 +1000

    documentation

---
+++ documentation/src/views/components/Chart.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import ChartCode from '../code/ChartCode.vue'
+import sourceCode from '../code/ChartCode.vue?raw'
+import RenderPreviewCode from '../../components/RenderPreviewCode.vue'
+</script>
+
+<template>
+  <div>
+    <div>
+      <p>
+        This is a custom set of reusable components built from
+        <a href="https://unovis.dev/" class="text-primary underline font-medium">
+          unovis
+        </a>.
+        Right now it offers basic support for a line chart. Everything can be used more or less like
+        unovis in its raw state, passing props and component structure.
+      </p>
+
+      <h3>Points</h3>
+      <ul class="list-disc list-inside">
+        <li>
+          All charts must be wrapped in `ChartProvider`
+        </li>
+        <li>
+          Expansion of support is planned, but feel free to make those contributions yourself
+        </li>
+        <li>
+          Checkout the documentation
+          <a href="https://unovis.dev/contributing/intro" class="text-primary underline font-medium">
+            here
+          </a>
+        </li>
+      </ul>
+    </div>
+    <RenderPreviewCode :source-code="sourceCode">
+      <ChartCode />
+    </RenderPreviewCode>
+  </div>
+</template>

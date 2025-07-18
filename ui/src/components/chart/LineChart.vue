b409d2352819509f9ff98c2f4a73220a2e9c2120
SethSharp <58869086+SethSharp@users.noreply.github.com>
2025-07-18 19:25:14 +1000

    wip

--- ui/src/components/chart/LineChart.vue
+++ ui/src/components/chart/LineChart.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
+import { VisTooltip } from '@unovis/vue'
 import { ChartProvider, Line, XAxis, YAxis, Legend } from '.'
+import { Line as LineType } from '@unovis/ts'
 
 const props = defineProps<{
   data: any
@@ -20,13 +22,21 @@ const lines = props.categories.map((key, i) => ({
   // todo: how can we dynamically apply other props to pass per line?
 }))
 
+const triggers = {
+  [LineType.selectors.line]: (d) => {
+    console.log(d)
+    return `<span>${d.x}, ${d.y} </span>`
+  }
+}
 </script>
 
 <template>
-  <div>
+  <div class="size-full">
     <ChartProvider :data>
       <XAxis label="X Axis" position="bottom" />
       <YAxis label="Y Axis" position="left" />
+
+      <VisTooltip :triggers="triggers" />
 
       <Line
         :x="x"
